ec624dd0bfc58364c6a66d7882818b5450ac1418
Prathin <pratinrp@gmail.com>
2025-07-12 19:52:47 +0530

    changed made in LinkedList and added BST

---
+++ LinkedListCode/LinkedListRev.java
@@ -0,0 +1,125 @@
+class Node {
+    int val;
+    Node next;
+
+    public Node() {
+    };
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    public void insertFirst(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+        if (tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void insertLast(int val) {
+        if (head == null) {
+            insertFirst(val);
+            return;
+        }
+        Node newNode = new Node(val);
+        tail.next = newNode;
+        tail = newNode;
+        size++;
+    }
+
+    public void insert(int val, int index) {
+        if (head == null) {
+            insertFirst(val);
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 2; i++) {
+            temp = temp.next;
+        }
+        Node newNode = new Node(val, temp.next);
+        temp.next = newNode;
+        size++;
+    }
+
+    public Node getReferenceOfNode(int index) {
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public int deleteFirst() {
+        int deletedVal = head.val;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        System.out.println("Deleted value: " + deletedVal);
+        return deletedVal;
+    }
+
+    public int deleteLast() {
+        Node temp = new Node();
+        for (int i = 0; i < size - 2; i++) {
+            temp = temp.next;
+        }
+        tail = temp;
+        int deletedVal = temp.next.val;
+        temp.next = null;
+        size--;
+        System.out.println("Deleted Value: " + deletedVal);
+        return deletedVal;
+    }
+
+    public int deleteInParticular(int index) {
+        if (index == 1) {
+            deleteFirst();
+            return 0;
+        } else if (index == size) {
+            deleteLast();
+            return 0;
+        }
+        Node temp = getReferenceOfNode(index - 1);
+        int deletedVal = temp.next.val;
+        temp.next = temp.next.next;
+        System.out.println("Deleted Value: " + deletedVal);
+        return deletedVal;
+    }
+
+    public void displayList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val);
+            if (temp.next != null) {
+                System.out.print(" -> ");
+            }
+            temp = temp.next;
+        }
+        return;
+    }
+}
+
+public class LinkedListRev {
+    public static void main(String[] args) {
+        LinkedList l1 = new LinkedList();
+        l1.insertFirst(2);
+        l1.insertFirst(1);
+        l1.displayList();
+    }
+}
---
+++ TreeCode/BinarySearchTree.java
@@ -0,0 +1,256 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/* 1) in binary tree, we are just going to pass values using method called insert. 
+ *    but the insertion operation is going to be done by helper method (eg: insertRec)
+ * 
+ * 2) same goes for delete operation.
+ * 
+ */
+class Node {
+    int val;
+    Node left;
+    Node right;
+    public Node next;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int data, Node next2) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+class Bst {
+    Node root;
+
+    public Bst() {
+        root = null;
+    }
+
+    public void insert(int val) { // not passing local variable so dont get confused.
+        root = insertNode(root, val);
+        return;
+    }
+
+    public Node insertNode(Node root, int val) { // helper method to insert node in binary tree
+        if (root == null) {
+            return new Node(val);
+        }
+        if (val < root.val) {
+            root.left = insertNode(root.left, val);
+        } else if (val > root.val) {
+            root.right = insertNode(root.right, val);
+        }
+        return root;
+    }
+
+    /*
+     * 1) this method is to assign a leaf node (with no children) to root node when
+     * the node with two children is to be removed
+     * 
+     * 2) two conditions because what if the root==null. that means there will be no
+     * left or right. (leads to null pointer exception)
+     * 
+     * 3) so i cant only check if minNode.left!=null but instead i should check both
+     * node and its left.
+     */
+    public Node minValueNode(Node root) {
+        Node minNode = root;
+        while (minNode != null && minNode.left != null) {
+            minNode = minNode.left;
+        }
+        return minNode;
+    }
+
+    public void remove(int val) {
+        root = removeNode(root, val);
+        return;
+    }
+
+    public Node removeNode(Node root, int target) { // except removing root value
+        if (root == null) {
+            return root;
+        }
+        if (target < root.val) {
+            root.left = removeNode(root.left, target);
+        } else if (target > root.val) {
+            root.right = removeNode(root.right, target);
+        } else {
+            if (root.left == null && root.right == null) {
+                // root = null;
+                return null;
+            } else if (root.left == null) {
+                // root = root.right;
+                // return root;
+                return root.right;
+            } else if (root.right == null) {
+                // root = root.left;
+                // return root;
+                return root.left;
+            }
+            /*
+             * remember that this is not a self balancing tree.
+             * so i took from root's right subtree
+             * we can also take root.left by taking the maximum value of root's left subtree
+             */
+            else {
+                Node minimumNode = minValueNode(root.right);
+                root.val = minimumNode.val; // so far, ive only copied the minimum value to the root node.
+                // the below line will remove the desired node
+                root.right = removeNode(root.right, minimumNode.val);
+            }
+        }
+        return root; // root node of the tree
+    }
+
+    /*
+     * ALL THREE ALGORITHMS ARE DEPTH FIRst SEARCH (DFS)
+     * 
+     * ALL THREE TRAVERSALS WILL REACH LEAF NODE (NODE WITHOUT CHILD) AND ONLY THEN
+     * IT GOES TO OTHER NODE.
+     * 
+     */
+    // in order traversal (left, root, right) -> same as sorted array
+    public void inorderTraversalDisplay() {
+        System.out.print("Inorder Traversal: ");
+        inorderTraversal(root);
+    }
+
+    public Node inorderTraversal(Node root) {
+        if (root == null) {
+            return root;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.val + " ");
+        inorderTraversal(root.right);
+        return root;
+    }
+
+    // pre order traversal (root,left,right)
+    public void preorderTraversalDisplay() {
+        System.out.print("Preorder Traversal: ");
+        preorderTraversal(root);
+    }
+
+    public Node preorderTraversal(Node root) {
+        if (root == null) {
+            return root;
+        }
+        System.out.print(root.val + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return root;
+
+    }
+
+    // post order traversal (left, right, root)
+    public void postorderTraversalDisplay() {
+        System.out.print("Postorder Traversal: ");
+        postorderTraversal(root);
+    }
+
+    public Node postorderTraversal(Node root) {
+        if (root == null) {
+            return root;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.print(root.val + " ");
+        return root;
+
+    }
+
+    // BREADTH FIRst SEARCH
+    public void levelOrderTraversalDisplay() {
+        levelOrderTraversal(root);
+    }
+
+    public void levelOrderTraversal(Node root) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        int curLevel = 0;
+        while (!queue.isEmpty()) {
+            // at first the size of queue is one because it has only one Node which is root
+            int len = queue.size();
+            System.out.print("Level: " + curLevel);
+            for (int i = 0; i < len; i++) {
+                /*
+                 * similar to pop(), it removes the first element (FIFO).
+                 * im assigning root to the node i.e node=root;
+                 */
+                Node node = queue.poll();
+                System.out.print(" -> " + node.val);
+
+                // by offering the node.left in queue, the size of queue increases dynamically
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            curLevel++;
+            System.out.println();
+        }
+    }
+
+    /*
+     * ONE MIstAKE IVE MADE WHILE TYPING THIS
+     * when you want to check using boolean, you need to return the result of called
+     * method.
+     * 
+     * if you type istargetPresent(root.right,target) instead of
+     * return istargetPresent(root.right, target), even if the element is present
+     * and it returns true to the method which called it, to the methods which are
+     * CALLED BEFORE wont be able to fetch it.
+     * 
+     * as a result, the method will return false which is the default value of
+     * boolean
+     * 
+     */
+    public void search(int target) {
+        if (istargetPresent(root, target)) {
+            System.out.println("The target element: " + target + " is present");
+        } else {
+            System.out.println("The target element: " + target + " is absent");
+        }
+    }
+
+    public boolean istargetPresent(Node root, int target) {
+        if (root == null) {
+            return false;
+        }
+        if (target > root.val) {
+            return istargetPresent(root.right, target);
+        } else if (target < root.val) {
+            return istargetPresent(root.left, target);
+        } else {
+            return true;
+        }
+    }
+}
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        Bst obj1 = new Bst();
+        obj1.insert(4);
+        obj1.insert(3);
+        obj1.insert(2);
+        obj1.insert(6);
+        obj1.insert(5);
+        obj1.insert(7);
+        obj1.inorderTraversalDisplay();
+        System.out.println("\n");
+        obj1.preorderTraversalDisplay();
+        System.out.println("\n");
+        obj1.postorderTraversalDisplay();
+        System.out.println("\n");
+        obj1.search(7);
+        System.out.println("\n");
+        obj1.levelOrderTraversalDisplay();
+    }
+}
